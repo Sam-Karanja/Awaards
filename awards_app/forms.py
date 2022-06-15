b6fb73eb155003a50ddf3a641e6e75fd0079c863
Moringa School <karanjasam210@gmail.com>
2022-06-15 10:18:41 +0300

    add form

---
+++ awards_app/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import Profile, Post, Rating
+from django.forms.widgets import Testarea
+
+
+class NewUserForm(UserCreationForm):
+    email=forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        field=("username", "email" , "password1","password2")
+
+
+    def save(self,commit=True):
+        user= super(NewUserForm,self).save(commit = False)
+        user.email = self.cleaned_date['email']
+        if commit:
+            user.save()
+        return user
