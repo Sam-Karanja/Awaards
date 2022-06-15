06c610cc2aed1da79717abf8305d47ddcca9514b
Moringa School <karanjasam210@gmail.com>
2022-06-15 15:51:56 +0300

    display profile

--- awards_app/forms.py
+++ awards_app/forms.py
@@ -1,3 +1,4 @@
+from binascii import a2b_base64
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -33,3 +34,21 @@ class RatingForm(forms.ModelForm):
     class Meta:
         model = Rating
         fields = ('design', 'usability', ' content')
+
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=250, help_text='Required. Enter a valid email address')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'profile_pic', 'bio', 'phone_number']
+
+        widgets =  {
+            'bio':Textarea(attrs={'cols':20, 'rows':5}),
+        }
